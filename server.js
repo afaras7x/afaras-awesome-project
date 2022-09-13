ca03197d5dd9cad3483d90879ecc966e497ce466
afaras7x <107579297+afaras7x@users.noreply.github.com>
2022-09-13 14:53:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello Awesome')
+    res.end('Hello Awesome this includes continuous deployment to cloud run amao great')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => console.log('Listening'))
